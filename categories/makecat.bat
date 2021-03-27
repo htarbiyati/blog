d404e8ef9a96c5dc6feaad81c2702f5e3c711831
htarbiyati <h.tarbiyati@gmail.com>
2021-03-28 02:19:51 +0430

    Removed All

--- categories/makecat.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-set filename=%1
-(
-ECHO ---
-ECHO layout: category
-ECHO title: %filename%
-ECHO category: %filename%
-ECHO ---
-) > %filename%.html
--- tags/maketag.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-set filename=%1
-(
-ECHO ---
-ECHO layout: tag
-ECHO title: %filename%
-ECHO tag: %filename%
-ECHO ---
-) > %filename%.html
